7a6e9568d4f1b0c1d4c7f60c91ca961dc5e7f17f
Maron <98313426+MaronHatoum@users.noreply.github.com>
2022-05-23 17:01:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 # Nothing to see here, just a test
-module "test" {
+module "test-fail" {
   source = "git@my-gitlab-instance.local:devops/terraform/instance.git?ref=v5.0.0"
 }
+  
+module "test-pass" {
+    source = "https://my-gitlab-instance.local/devops/terraform/instance.git?ref=v5.0.0"
+}
+
